7690159004b95f9c96b4169f00c8ffae4580f0f1
Scrimint, Nicoleta <nicoleta.scrimint@centric.eu>
2024-03-23 05:18:26 +0200

    Add unit tests

--- 8.DeveloperTesting/BooksLibrary/BooksLibrary.Domain.UnitTests/BookTests.cs
+++ 8.DeveloperTesting/BooksLibrary/BooksLibrary.Domain.UnitTests/BookTests.cs
@@ -1,11 +1,25 @@
-namespace BooksLibrary.Domain.UnitTests
-{
-    public class BookTests
-    {
-        [Fact]
-        public void Test1()
-        {
+using FluentAssertions;
 
-        }
+namespace BooksLibrary.Domain.UnitTests;
+
+public class BookTests
+{
+    [Fact]
+    public void Should_ReturnFailure_When_CreateBookWithExistingTitle()
+    {
+        // Arrange
+        var authors = new List<Author>
+        {
+            new("Rob", "Vettor")
+        };
+        var bookTitle = "Architecting Cloud Native .NET Applications for Azure";
+        var existingTitles = new List<string> {bookTitle};
+
+        // Act
+        var result = Book.CreateBook(authors, new DateOnly(2024, 1, 2), "Architecting Cloud Native .NET Applications for Azure", existingTitles);
+
+        // Assert
+        result.IsFailure.Should().BeTrue();
+        result.Error.Should().Be($"Book with title '{bookTitle}' already exists.");
     }
 }
--- 8.DeveloperTesting/BooksLibrary/BooksLibrary.Domain/Book.cs
+++ 8.DeveloperTesting/BooksLibrary/BooksLibrary.Domain/Book.cs
@@ -21,23 +21,3 @@ public class Book(Guid id, List<Author> authors, DateOnly publicationDate, strin
 
     public string Title { get; private set; } = title;
 }
-
-public class Result<T>
-    where T : class?
-{
-    private Result(T value, bool isSuccess, string error)
-    {
-        Value = value;
-        IsSuccess = isSuccess;
-        Error = error;
-    }
-
-    public bool IsSuccess { get; private set; }
-    public bool IsFailure => !IsSuccess;
-    public T? Value { get; private set; }
-    public string Error { get; private set; }
-
-    public static Result<T> Success(T value) => new(value, true, string.Empty);
-
-    public static Result<T?> Failure(string error) => new(null, false, error);
-}
