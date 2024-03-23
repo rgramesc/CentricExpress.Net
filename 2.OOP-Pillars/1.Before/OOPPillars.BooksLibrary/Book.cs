c03754505f5636f537e8bb6069bee94fddac3ec7
EGrigore <EGrigore@NBKR004475>
2024-03-23 06:51:43 +0200

    OOP pillars exercises, modify static polymorphism

--- 2.OOP-Pillars/1.Before/OOPPillars.BooksLibrary/Book.cs
+++ 2.OOP-Pillars/1.Before/OOPPillars.BooksLibrary/Book.cs
@@ -1,11 +1,9 @@
 ﻿namespace CentricExpress.OOPPillars.BooksLibrary.Models
 {
     public abstract class Book
-    {
-        private Guid _id { get; set; }
+    {        
         public Author[] Authors { get; set; }
-        public string Title { get; set; }
-        public string ISBN { get; private set; }
+        public string Title { get; set; }        
 
         protected Book(Author[] authors, string title)
         {
@@ -13,19 +11,18 @@
             Title = title;
         }
 
-        public virtual void GenerateISBN()
-        {
-            _id = Guid.NewGuid();
-            ISBN = $"PUB-{_id}";
-        }
-
-        public virtual void GenerateISBN(Guid id)
-        {
-            _id = id;
-            ISBN = $"PUB-{id}";
-        }
-
         public abstract string AccessContent(string username);
-        
+
+        public virtual void PrintBook()
+        {
+            var authors = "";
+            foreach (var author in Authors)
+            {
+                authors += $"{author.LastName}, {author.FirstName}";
+            }
+
+            Console.WriteLine($"{Title} - {authors}");
+        }
+
     }
 }
---
+++ 2.OOP-Pillars/2.After/OOPPillars.BooksLibrary/BookLibrary.cs
@@ -0,0 +1,52 @@
+﻿using CentricExpress.OOPPillars.BooksLibrary.Models;
+
+namespace OOPPillars.BooksLibrary
+{
+    public class BookLibrary
+    {
+        public IList<Book> Books { get; private set; }
+
+        public BookLibrary()
+        {
+            Books = new List<Book>();
+
+            //add some hardcoded values in the book collection, for demo purposes
+            AddBooks();
+        }
+        
+        public IList<Book> SearchBook(string title)
+        { 
+            var result = Books.Where(book => book.Title.Contains(title, StringComparison.OrdinalIgnoreCase)).ToList();
+            return result;
+        }
+
+        public IList<Book> SearchBook(string title, string author)
+        {
+            var result = Books.Where(book => book.Title.Contains(title, StringComparison.OrdinalIgnoreCase) && 
+                                             book.Authors.Any(auth => auth.FirstName.Contains(author, StringComparison.OrdinalIgnoreCase) || 
+                                                                      auth.LastName.Contains(author, StringComparison.OrdinalIgnoreCase)))
+                              .ToList();
+            return result ?? new List<Book>();
+        }
+
+        private void AddBooks()
+        {
+            var specialAuthors = new Author[]
+                        {
+                new Author("Sebastian", "Münster")
+                        };
+
+            Book specialBook = new SpecialCollectionBook(specialAuthors, "Cosmografia");
+            Books.Add(specialBook);
+
+            var publicAuthors = new Author[]
+            {
+                new Author("Martin", "Fowler")
+            };
+
+            Book publicBook = new PublicBook(publicAuthors, "Patterns of Enterprise Architecture");
+            Books.Add(publicBook);
+        }
+
+    }
+}
